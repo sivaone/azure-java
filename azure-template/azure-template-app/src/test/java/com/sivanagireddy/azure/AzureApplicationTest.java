915d5baa2a47c029fc76757b4e15930a55ca7e15
siva nagi reddy alla <siva.alla@outlook.com>
2022-07-08 12:46:39 +0530

    Initial commit with tempalte module

---
+++ azure-template/azure-template-app/src/test/java/com/sivanagireddy/azure/AzureApplicationTest.java
@@ -0,0 +1,14 @@
+package com.sivanagireddy.azure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class AzureApplicationTest {
+
+    @Test
+    void contextLoads() {
+
+    }
+}
